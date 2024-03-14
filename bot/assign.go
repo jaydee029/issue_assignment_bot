4b366f91778ffd7f54441dd2b724ed19574ce2ca
dhruv <dhruvj797@gmail.com>
2024-03-14 17:15:42 +0530

    1.1.2
    Signed-off-by: dhruv <dhruvj797@gmail.com>

--- bot/assign.go
+++ bot/assign.go
@@ -11,15 +11,17 @@ type Input struct {
 
 func Bot(input Input) {
 
-	event := githubactions.GitHubContext{}
-	if event.EventName == "issue_comment" {
-		githubactions.Debugf("Hello")
-		//githubactions.Fatalf("This action is only supported for issue_comment event")
-	}
-	/*
-		client := github.NewClient(nil)
-		// list all organizations for user "willnorris"
-		orgs, _, _ := client.Organizations.List(context.Background(), "hydai", nil)
-		fmt.Println("orgs:", orgs)
-	*/
+	githubactions.Infof("Hello")
+
+	//event := githubactions.GitHubContext{}
+	//if event.EventName == "issue_comment" {
+
+	//githubactions.Fatalf("This action is only supported for issue_comment event")
 }
+
+/*
+	client := github.NewClient(nil)
+	// list all organizations for user "willnorris"
+	orgs, _, _ := client.Organizations.List(context.Background(), "hydai", nil)
+	fmt.Println("orgs:", orgs)
+*/
